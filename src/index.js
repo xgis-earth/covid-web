06b22ad38b35ff7ff0efc17891b3f490d811b667
Steven Robertson <steverobertson@gmail.com>
2020-12-21 21:11:53 +0000

    Removing the react-scroll-manager package

--- src/index.js
+++ src/index.js
@@ -5,18 +5,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {Provider as ReduxProvider} from "react-redux";
 import {ConnectedRouter} from "connected-react-router";
-import {ScrollManager, WindowScroller} from "react-scroll-manager";
 import {history, store} from "./redux/store";
 import App from "./components/App";
 
 ReactDOM.render(
-    <ScrollManager history={history}>
-        <ReduxProvider store={store}>
-            <ConnectedRouter history={history}>
-                <WindowScroller>
-                    <App/>
-                </WindowScroller>
-            </ConnectedRouter>
-        </ReduxProvider>
-    </ScrollManager>,
+    <ReduxProvider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </ReduxProvider>,
     document.getElementById('root'));
