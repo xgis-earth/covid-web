0e1f9ff05bd159af3959f46db3b0a2b163abe5db
Steven Robertson <steverobertson@gmail.com>
2020-12-22 18:56:25 +0000

    Data attribution: JHU CSSE COVID-19 Data

--- src/components/DataAttribution.js
+++ src/components/DataAttribution.js
@@ -4,9 +4,21 @@ class DataAttribution extends React.Component {
 
     render() {
         return (
-            <div className="page-margin page-max-width">
-                <div className="alert alert-warning" role="alert">
-                    TODO: Attribute all data sources here.
+            <div style={{margin: "12px"}}>
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb">
+                        <li className="breadcrumb-item active">Data Sources</li>
+                    </ol>
+                </nav>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">JHU CSSE COVID-19 Data</h5>
+                        <p className="card-text">COVID-19 Data Repository by the Center for Systems Science and
+                            Engineering (CSSE) at Johns Hopkins University.</p>
+                        <a href="https://github.com/CSSEGISandData/COVID-19" className="btn btn-primary">
+                            Go to CSSEGISandData/COVID-19 on GitHub
+                        </a>
+                    </div>
                 </div>
             </div>
         )
